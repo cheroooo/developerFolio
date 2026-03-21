8c97793fbfc1162e9d25fb7b7769566dc1230227
cheroooo <cherry.zhu5.26@gmail.com>
2026-03-21 19:06:36 -0400

    Update Contact.js

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -52,16 +52,7 @@ export default function Contact() {
               <SocialMedia />
             </div>
           </div>
-          <div className="contact-image-div">
-            {illustration.animated ? (
-              <DisplayLottie animationData={email} />
-            ) : (
-              <img
-                alt="Man working"
-                src={require("../../assets/images/contactMailDark.svg")}
-              ></img>
-            )}
-          </div>
+
         </div>
       </div>
     </Fade>
