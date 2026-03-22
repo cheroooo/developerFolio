455f753c1fe76211f73c0b0cf56199d6881042d7
cheroooo <cherry.zhu5.26@gmail.com>
2026-03-21 21:08:49 -0400

    Update HrOperations.js

--- src/containers/HrOperations/HrOperations.js
+++ src/containers/HrOperations/HrOperations.js
@@ -13,7 +13,31 @@ export default function HrOperations() {
               HR Operations
             </h1>
             <p className={isDark ? "dark-mode-text" : "subTitle"}>
-              Supporting cross functional leaders and teams, curating a personal touch workplace experience, and optimizing daily workflows to drive continuous improvement.
+              Supporting cross functional leaders and teams, curating a high touch workplace experience, and optimizing daily workflows to drive continuous improvement. 
+
+
+                Recruitment - The experience for The recruitment process is a preview of what it’s like to work at the company. Personalizing the interaction set a positive impression for the candidate.
+                <br />
+                |
+                Onboarding - A clear plan, resources, and genuine support help new colleagues settle into their roles.
+                <br />
+                |
+                Performance Review - A reflection of an individual's accomplishments and skills, helping them succeed and grow within the organization in the long term. 
+                <br />
+                |
+                Employee Engagement - Encourage individuals to take ownership of their professional growth and recognition within the organization.
+                <br />
+                |
+                Data Analysis - Generate report 
+                
+                Offboarding - Understand employee departures and ensure a smooth transition.
+          
+  
+
+
+
+
+                
             </p>
           </div>
         </Fade>
