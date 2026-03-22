a8f931a54c362ebca8761c9cb82271bea6bda67e
Cherry Zhu <fengyang@pop-os.localdomain>
2026-03-22 18:45:28 -0400

    reformat

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Main />
     </BrowserRouter>
   );
