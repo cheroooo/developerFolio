588eb55a5597d8a7f55c5020e7307177ed387bd0
Cherry Zhu <fengyang@pop-os.localdomain>
2026-03-22 13:19:13 -0400

    reformat

--- src/containers/ProgramManagement/ProgramManagement.js
+++ src/containers/ProgramManagement/ProgramManagement.js
@@ -1,9 +1,30 @@
 import React, { useContext } from "react";
 import StyleContext from "../../contexts/StyleContext";
 import { Fade } from "react-reveal";
+import "./ProgramManagement.scss";
 
 export default function ProgramManagement() {
   const { isDark } = useContext(StyleContext);
+
+  const programs = [
+    {
+      title: "Career Fairs",
+      desc: "Encourage internal teams to represent the firm at university career fairs, forging connections with students to build a robust early career talent pipeline."
+    },
+    {
+      title: "Externship Program",
+      desc: "Manage the end to end externship program process. This ranges from allocating headcounts, collaborating with the marketing team, and facilitating the candidate review process."
+    },
+    {
+      title: "Internship Program",
+      desc: "Design an immersive, hands-on internship program by partnering with the planning committee to curate weekly learning sessions focused on industry knowledge and team building in 10 weeks. The program satisfaction rate is maintained at 94% every year."
+    },
+    {
+      title: "Year One Program",
+      desc: "Oversee emerging talent pipelines across 5+ disciplines. My core focus is ensuring diversity by strategically distributing candidates across backgrounds, while owning the recruitment process (interview scheduling, offer extensions, and onboarding). 9 out of 10 interns reapply to the full time opportunity after graduation."
+    }
+  ];
+
   return (
     <div className="main" id="program-management">
       <div className="program-management-container">
@@ -14,17 +35,20 @@ export default function ProgramManagement() {
             </h1>
             <p className={isDark ? "dark-mode-text" : "subTitle"}>
               From organizing career fairs and engineering internship/externship programs to managing full time early career pipelines, my work supports students and recent grads in exploring career paths within the industry.
-                <br />
-                Career Fairs: Encourage internal teams to represent the firm at university career fairs, forging connections with students to build a robust early career talent pipeline. 
-              <br />
-                Externship Program: Manage the end to end externship program process. This ranges from allocating headcounts, collaborating with the marketing team, and facilitating the candidate review process. 
-              <br /> 
-                Internship Program: Designe an immersive, hands-on internship program by partnering with the planning committee to curate weekly learning sessions focused on industry knowledge and team building in 10 weeks. The program satisfaction rate is maintained at 94% every year. 
-              <br />
-                Year One Program: OVersee emerging talent pipelineS across 5+ disciplines. My core focus is ensuring diversity by strategically distributing candidates across backgrounds, while owning the recruitment process (interview scheduling, offer extensions, and onboarding). 9 out of 10 interns reapply to the full time opportunity after graduation. 
             </p>
           </div>
         </Fade>
+        
+        <div className="prog-mgmt-cards-div">
+          {programs.map((prog, i) => (
+            <Fade bottom duration={1000} distance="40px" key={i}>
+              <div className={isDark ? "prog-mgmt-card-dark" : "prog-mgmt-card"}>
+                <h3 className={isDark ? "dark-mode-title" : "prog-mgmt-card-title"}>{prog.title}</h3>
+                <p className={isDark ? "dark-mode-text" : "prog-mgmt-card-text"}>{prog.desc}</p>
+              </div>
+            </Fade>
+          ))}
+        </div>
       </div>
     </div>
   );
