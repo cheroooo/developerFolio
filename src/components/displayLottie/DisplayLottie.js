aef6ac1abeb2b73907f59bd708063210720e77b9
cheroooo <cherry.zhu5.26@gmail.com>
2026-03-21 23:45:59 -0400

    Update DisplayLottie.js

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -16,6 +16,7 @@ export default class DisplayLottie extends Component {
         <Lottie
           animationData={defaultOptions.animationData}
           loop={defaultOptions.loop}
+          style={{ height: "65%", width: "65%" }}
         />
       </Suspense>
     );
